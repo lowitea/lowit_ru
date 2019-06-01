c94c418b3dbd168f351fa1c1b239fd119fbb6ad9
Evgeniy <ea@lowit.ru>
2019-06-01 22:30:46 +0300

    добавляет html версию страницы

--- index.php
+++ index.php
@@ -1,53 +1,79 @@
-<?php
-function random_color_part() {
-    return str_pad( dechex( mt_rand( 0, 255 ) ), 2, '0', STR_PAD_LEFT);
-}
-
-function random_color() {
-    return random_color_part() . random_color_part() . random_color_part();
-}
-function hexrgb_invert($hex) {
-    $arr = str_split($hex, 2);
-    foreach ($arr as &$value) {
-        $c = base_convert($value, 16, 10);
-        $value = str_pad(base_convert(255 - $c, 10, 16), 2, '0', STR_PAD_LEFT);
-    }
-    return implode('', $arr);
-}
-
-$BASE_COLOR = random_color();
-$INVERT_COLOR = hexrgb_invert($BASE_COLOR);
-?>
-
-
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <title>lowit</title>
+  <meta name="author" content="Evgeniy Mitin <ea@lowit.ru>">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width,
+                 user-scalable=no,
+                 initial-scale=1.0,
+                 maximum-scale=1.0,
+                 minimum-scale=1.0"
+  >
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?
+              family=Roboto:400,900&display=swap"
+        rel="stylesheet">
 
-    <title>Hello, world!</title>
-
-    <style>
-        body {
-            background: #<?php echo $BASE_COLOR; ?>;
-        }
-    </style>
-  </head>
-  <body>
-    <div class="container-fluid">
-     <h1>Hello, world!</h1>
-     <p style="color: #<?php echo $INVERT_COLOR ?>">Hello world</p>
-    </div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+  <style>
+    .blink {
+      animation: blink 1500ms linear infinite;
+    }
+    @keyframes blink {
+      0% { opacity: 0; }
+      10% { opacity: 0; }
+      35% { opacity: 1; }
+      65% { opacity: 1; }
+      90% { opacity: 0; }
+      100% { opacity: 0; }
+    }
+    body {
+      background-color: #263238;
+      color: #ffffff;
+      font-family: 'Roboto', sans-serif;
+    }
+    p::before {
+      content: "guest@lowit_personal_page:~$ ";
+      opacity: 0.6;
+    }
+    a {
+      color: #ffffff;
+    	transition: all 200ms ease-in;
+      text-decoration: none;
+      outline : none;
+    }
+    a:hover{
+      opacity: 0.6;
+    }
+    .qr_code {
+      opacity: 0.8;
+    }
+  </style>
+</head>
+<body>
+<p>
+  show&nbsp;email
+  <br>
+  <a href="mailto:ea@lowit.ru">ea@lowit.ru</a>
+  <br>
+  <img class="qr_code" src="qr_codes/email.svg">
+</p>
+<p>
+  show&nbsp;telegram
+  <br>
+  <a href="tg://resolve?domain=lowit">@lowit</a>
+  <br>
+  <img class="qr_code" src="qr_codes/telegram.svg">
+</p>
+<p>
+  show&nbsp;this&nbsp;site
+  <br>
+  <a href="http://lowit.ru">lowit.ru</a>
+  <br>
+  <img class="qr_code" src="qr_codes/site.svg">
+</p>
+<p><span style="font-weight: 900;" class="blink">|</span></p>
+</body>
 </html>
